f7fb513d1c7616208351ee626e26b150b77c0518
zajtsev <dimazaicev88@gmail.com>
2025-05-27 17:48:15 +0300

    fix changes

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -1,9 +1,9 @@
 <?php return array(
     'root' => array(
         'name' => '__root__',
-        'pretty_version' => '1.0.0+no-version-set',
-        'version' => '1.0.0.0',
-        'reference' => null,
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => 'f68e397d8cafa166f865b4fe79b3fbecc2eca030',
         'type' => 'library',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -11,9 +11,9 @@
     ),
     'versions' => array(
         '__root__' => array(
-            'pretty_version' => '1.0.0+no-version-set',
-            'version' => '1.0.0.0',
-            'reference' => null,
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => 'f68e397d8cafa166f865b4fe79b3fbecc2eca030',
             'type' => 'library',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
